2ec5ba092fef1da86d8709b8be558f109efd743e
Yvonne <yvonne@visitor-43498947.dyndns.cern.ch>
2019-02-14 10:34:18 +0100

    saving changes in keras

--- Setup.sh
+++ Setup.sh
@@ -2,5 +2,5 @@
 # Fixed
 #export PYTHONPATH="/Users/yvonne/Projects/scikitGP/":$PYTHONPATH
 # poiting to the gpml location in the backend
-#export GPML_PATH=/Users/yvonne/Projects/DKLKerasGP/kgp/kgp/backend
-export GPML_PATH=./gpml:./keras-gp/kgp/backend/utils
+export GPML_PATH=/Users/yvonne/Projects/DKLKerasGP/kgp/kgp/backend:$GPML_PATH
+#export GPML_PATH=$(pwd)/gpml:$(pwd)/keras-gp/kgp/backend/utils
